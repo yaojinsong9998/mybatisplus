f56f17aa529b1e86a1f729cae82d9f3cf42a8a38
yaojinsong <924602776@qq.com>
2022-10-31 21:28:15 +0800

    '分库'

---
+++ mybatis_plus_database/src/test/java/com/atguigu/mybatisplus/MybatisPlusDatabaseApplicationTests.java
@@ -0,0 +1,26 @@
+package com.atguigu.mybatisplus;
+
+import com.atguigu.mybatisplus.service.ProductService;
+import com.atguigu.mybatisplus.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    public void test(){
+        System.out.println(userService.getById(1));
+        System.out.println(productService.getById(1));
+    }
+}
