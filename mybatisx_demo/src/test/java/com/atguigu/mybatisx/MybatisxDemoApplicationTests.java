77fee027502a07297a2f2f0866b6bd53bda03715
yaojinsong <924602776@qq.com>
2022-10-31 22:11:23 +0800

    'mybatisx'

---
+++ mybatisx_demo/src/test/java/com/atguigu/mybatisx/MybatisxDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.atguigu.mybatisx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
